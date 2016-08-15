4b7ba174eccd45438612ac9b18a7225de5ea9235
root <git4bytter@sina.cn>
2016-08-15 14:03:35 +0800

    task-080-ex01

---
+++ src/main/java/com/betterjr/modules/blacklist/constant/BlacklistConstants.java
@@ -0,0 +1,22 @@
+package com.betterjr.modules.blacklist.constant;
+
+public interface BlacklistConstants {
+
+    /**
+     * 状态:0-未生效;
+     */
+    String BLACKLIST_STATUS_INEFFECTIVE = "0";
+    /**
+     * 状态:1-已生效;
+     */
+    String BLACKLIST_STATUS_EFFECTIVE = "1";
+    /**
+     * 类型:0-个人;
+     */
+    String BLACKLIST_TYPE_PERSONAL = "1";
+    /**
+     * 类型:1-机构;
+     */
+    String BLACKLIST_TYPE_BRANCHES = "1";
+
+}
